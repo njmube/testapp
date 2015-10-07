76d6c67f0bc5dbf45e3999c2794d04267c3ad926
Luis Macias <luismacias.angulo@gmail.com>
2015-10-07 11:54:54 -0500

    Trabajando con el login

--- app/src/main/java/com/app/luis/androidapp/LoginActivity.java
+++ app/src/main/java/com/app/luis/androidapp/LoginActivity.java
@@ -1,14 +1,8 @@
 package com.app.luis.androidapp;
 
-import android.app.Activity;
 import android.content.Intent;
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
-import android.content.pm.Signature;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Base64;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -23,15 +17,72 @@ import com.facebook.login.widget.LoginButton;
 import com.flaviofaria.kenburnsview.KenBurnsView;
 import com.flaviofaria.kenburnsview.Transition;
 
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements KenBurnsView.TransitionListener, View.OnClickListener {
 
     private EditText email, password;
     private Button entrar;
     private LoginButton loginButton;
     private CallbackManager callbackManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        callbackManager = CallbackManager.Factory.create();
+
+        setContentView(R.layout.login_activity);
+
+        email = (EditText) findViewById(R.id.editText_email);
+        password = (EditText) findViewById(R.id.editText_password);
+        entrar = (Button) findViewById(R.id.button_entrar);
+        loginButton = (LoginButton) findViewById(R.id.login_button);
+
+        // KenBurnsView de fondo
+        KenBurnsView kenBurnsView = (KenBurnsView) findViewById(R.id.backgroud);
+        kenBurnsView.setTransitionListener(this);
+
+        // Boton entrar
+        entrar.setOnClickListener(this);
+
+        // Login con Facebook
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Toast.makeText(LoginActivity.this, "User ID: "
+                        + loginResult.getAccessToken().getUserId()
+                        + "\n" +
+                        "Auth Token: "
+                        + loginResult.getAccessToken().getToken(), Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onCancel() {
+                Toast.makeText(LoginActivity.this, "Cancelado", Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Toast.makeText(LoginActivity.this, "Error: " + error.toString(), Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    // KenBurnsView de fondo
+    @Override
+    public void onTransitionStart(Transition transition) {}
+
+    // KenBurnsView de fondo
+    @Override
+    public void onTransitionEnd(Transition transition) {}
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.button_entrar) {
+            String email = this.email.getText().toString();
+            String password = this.password.getText().toString();
+            Toast.makeText(getApplicationContext(), "Email: " + email + "\nPassword: " + password, Toast.LENGTH_LONG).show();
+        }
+    }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -50,106 +101,5 @@ public class LoginActivity extends AppCompatActivity {
         super.onPause();
         // Logs 'app deactivate' App Event.
         AppEventsLogger.deactivateApp(this);
-    }
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        FacebookSdk.sdkInitialize(getApplicationContext());
-        callbackManager = CallbackManager.Factory.create();
-
-        setContentView(R.layout.login_activity);
-
-        email = (EditText) findViewById(R.id.editText_email);
-        password = (EditText) findViewById(R.id.editText_password);
-        entrar = (Button) findViewById(R.id.button_entrar);
-
-
-        loginButton = (LoginButton) findViewById(R.id.login_button);
-
-        // KenBurnsView de fondo
-        KenBurnsView kenBurnsView = (KenBurnsView) findViewById(R.id.backgroud);
-        kenBurnsView.setTransitionListener(new KenBurnsView.TransitionListener() {
-            @Override
-            public void onTransitionStart(Transition transition) {
-
-            }
-
-            @Override
-            public void onTransitionEnd(Transition transition) {
-
-            }
-        });
-
-        // Boton entrar
-        entrar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String semail = email.getText().toString();
-                String spassword = password.getText().toString();
-
-                printKeyHash(LoginActivity.this);
-
-                Toast.makeText(LoginActivity.this, "Email: "+semail+"\nPassword: "+spassword, Toast.LENGTH_LONG).show();
-            }
-        });
-
-        // Login con Facebook
-        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
-            @Override
-            public void onSuccess(LoginResult loginResult) {
-                Toast.makeText(LoginActivity.this,"User ID: "
-                        + loginResult.getAccessToken().getUserId()
-                        + "\n" +
-                        "Auth Token: "
-                        + loginResult.getAccessToken().getToken(), Toast.LENGTH_LONG).show();
-            }
-
-            @Override
-            public void onCancel() {
-                Toast.makeText(LoginActivity.this, "Cancelado", Toast.LENGTH_LONG).show();
-            }
-
-            @Override
-            public void onError(FacebookException error) {
-                Toast.makeText(LoginActivity.this, "Error: "+error.toString(), Toast.LENGTH_LONG).show();
-            }
-        });
-
-
-    }
-
-    public static String printKeyHash(Activity context) {
-        PackageInfo packageInfo;
-        String key = null;
-        try {
-            //getting application package name, as defined in manifest
-            String packageName = context.getApplicationContext().getPackageName();
-
-            //Retriving package info
-            packageInfo = context.getPackageManager().getPackageInfo(packageName,
-                    PackageManager.GET_SIGNATURES);
-
-            Log.e("Package Name=", context.getApplicationContext().getPackageName());
-
-            for (Signature signature : packageInfo.signatures) {
-                MessageDigest md = MessageDigest.getInstance("SHA");
-                md.update(signature.toByteArray());
-                key = new String(Base64.encode(md.digest(), 0));
-
-                // String key = new String(Base64.encodeBytes(md.digest()));
-                Log.e("Key Hash=", key);
-            }
-        } catch (PackageManager.NameNotFoundException e1) {
-            Log.e("Name not found", e1.toString());
-        }
-        catch (NoSuchAlgorithmException e) {
-            Log.e("No such an algorithm", e.toString());
-        } catch (Exception e) {
-            Log.e("Exception", e.toString());
-        }
-
-        return key;
     }
 }
