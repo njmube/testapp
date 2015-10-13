5982a0e9de9059796dfdca9f0a0059f21488542c
Luis Macias <luismacias.angulo@gmail.com>
2015-10-12 21:20:49 -0500

    Toolbar nuevo registro

--- app/src/main/java/com/app/luis/androidapp/activities/NuevaCuenta.java
+++ app/src/main/java/com/app/luis/androidapp/activities/NuevaCuenta.java
@@ -1,16 +1,50 @@
 package com.app.luis.androidapp.activities;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.app.luis.androidapp.R;
 
 public class NuevaCuenta extends AppCompatActivity {
+
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_nueva_cuenta);
 
+        toolbar = (Toolbar) findViewById(R.id.toolbar_actionbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        toolbar.setTitle("Registro");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_done, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_done_action:
+                Toast.makeText(getApplicationContext(), "Envia los datos", Toast.LENGTH_LONG).show();
+                break;
+
+            case android.R.id.home:
+                finish();
+                break;
+        }
+
+        return true;
     }
 }
