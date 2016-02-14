4d7ce68745dab9a9d79dc291625d749a47b8b898
Luis Macias <luismacias.angulo@gmail.com>
2016-02-14 13:58:44 -0600

    ButterKnife en Login

--- app/src/main/java/com/app/luis/androidapp/activities/Login.java
+++ app/src/main/java/com/app/luis/androidapp/activities/Login.java
@@ -1,24 +1,19 @@
 package com.app.luis.androidapp.activities;
 
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AlertDialog;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.text.InputType;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
-import android.widget.ViewSwitcher;
-
+import android.widget.*;
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import com.afollestad.materialdialogs.DialogAction;
 import com.afollestad.materialdialogs.MaterialDialog;
 import com.afollestad.materialdialogs.Theme;
 import com.app.luis.androidapp.R;
 import com.app.luis.androidapp.helpers.DataValidator;
-import com.app.luis.androidapp.helpers.Utils;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
@@ -29,13 +24,28 @@ import com.facebook.login.widget.LoginButton;
 import com.flaviofaria.kenburnsview.KenBurnsView;
 import com.flaviofaria.kenburnsview.Transition;
 
-public class Login extends AppCompatActivity implements KenBurnsView.TransitionListener, View.OnClickListener {
+public class Login extends AppCompatActivity implements KenBurnsView.TransitionListener {
 
     private static final int TRANSITIONS_TO_SWITCH = 3;
-    private EditText editTextEmail, editTextPassword, editTextEmailRestablece;
+    @Bind(R.id.viewSwitcher)
+    ViewSwitcher mViewSwitcher;
+    @Bind(R.id.bg_login)
+    KenBurnsView bgLogin;
+    @Bind(R.id.bg_login2)
+    KenBurnsView bgLogin2;
+    @Bind(R.id.editText_email)
+    EditText editTextEmail;
+    @Bind(R.id.editText_password)
+    EditText editTextPassword;
+    @Bind(R.id.button_entrar)
+    Button botonEntrar;
+    @Bind(R.id.textView_nueva_cuenta)
+    TextView textViewNuevaCuenta;
+    @Bind(R.id.textView_olvida_password)
+    TextView textViewOlvidaPassword;
+    @Bind(R.id.login_button)
+    LoginButton loginButton;
     private CallbackManager callbackManager;
-    private AlertDialog alertDialog;
-    private ViewSwitcher mViewSwitcher;
     private int mTransitionsCount = 0;
 
     @Override
@@ -43,29 +53,12 @@ public class Login extends AppCompatActivity implements KenBurnsView.TransitionL
         super.onCreate(savedInstanceState);
         FacebookSdk.sdkInitialize(getApplicationContext());
         callbackManager = CallbackManager.Factory.create();
-
         setContentView(R.layout.layout_login);
-
-        editTextEmail = (EditText) findViewById(R.id.editText_email);
-        editTextPassword = (EditText) findViewById(R.id.editText_password);
-
-        final Button buttonEntrar = (Button) findViewById(R.id.button_entrar);
-        final TextView textViewNuevaCuenta = (TextView) findViewById(R.id.textView_nueva_cuenta);
-        final TextView textViewOlvidaPassword = (TextView) findViewById(R.id.textView_olvida_password);
-        LoginButton loginButton = (LoginButton) findViewById(R.id.login_button);
+        ButterKnife.bind(this);
 
         // KenBurnsView de fondo
-        mViewSwitcher = (ViewSwitcher) findViewById(R.id.viewSwitcher);
-        KenBurnsView bgLogin = (KenBurnsView) findViewById(R.id.bg_login);
-        KenBurnsView bgLogin2 = (KenBurnsView) findViewById(R.id.bg_login2);
         bgLogin.setTransitionListener(this);
         bgLogin2.setTransitionListener(this);
-
-
-        // Botones
-        buttonEntrar.setOnClickListener(this);
-        textViewNuevaCuenta.setOnClickListener(this);
-        textViewOlvidaPassword.setOnClickListener(this);
 
         // Login con Facebook
         loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
@@ -90,6 +83,25 @@ public class Login extends AppCompatActivity implements KenBurnsView.TransitionL
         });
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // Logs 'install' and 'app activate' App Events.
+        AppEventsLogger.activateApp(this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        // Logs 'app deactivate' App Event.
+        AppEventsLogger.deactivateApp(this);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
     // KenBurnsView de fondo
     @Override
     public void onTransitionStart(Transition transition) {
@@ -106,121 +118,67 @@ public class Login extends AppCompatActivity implements KenBurnsView.TransitionL
         }
     }
 
-    @Override
-    public void onClick(View view) {
+    @OnClick(R.id.button_entrar)
+    public void entrarLogin() {
+        String email = this.editTextEmail.getText().toString();
+        String password = this.editTextPassword.getText().toString();
 
-        switch (view.getId()) {
-            case R.id.button_entrar:
-                String email = this.editTextEmail.getText().toString();
-                String password = this.editTextPassword.getText().toString();
+        Toast.makeText(getApplicationContext(), "Email: " + email + "\nPassword: " + password, Toast.LENGTH_LONG).show();
 
-                //Toast.makeText(getApplicationContext(), "Email: " + email + "\nPassword: " + password, Toast.LENGTH_LONG).show();
-
-                Intent intentHome = new Intent(this, Home.class);
-                startActivity(intentHome);
-
-                break;
-            case R.id.textView_nueva_cuenta:
-
-                Intent intentNuevaCuenta = new Intent(this, NuevaCuenta.class);
-                startActivity(intentNuevaCuenta);
-
-                break;
-            case R.id.textView_olvida_password:
-
-                new MaterialDialog.Builder(this)
-                        .title(R.string.DIALOG_TITULO_RESTABLECE_PASSWORD)
-                        .content("Escribe tu correo electrónico para enviarte un e-mail de recuperación.")
-                        .inputType(InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS)
-                        .positiveText(R.string.BUTTON_RESTABLECER)
-                        .positiveColorRes(R.color.primary)
-                        .alwaysCallInputCallback() // this forces the callback to be invoked with every input change
-                        .input(R.string.HINT_EMAIL, 0, false, new MaterialDialog.InputCallback() {
-                            @Override
-                            public void onInput(MaterialDialog dialog, CharSequence input) {
-                                if (emptyFieldDialog(input.toString())) {
-                                    dialog.setContent(R.string.INPUT_ERROR_CORREO_INVALIDO);
-                                    dialog.getActionButton(DialogAction.POSITIVE).setEnabled(false);
-                                } else {
-                                    dialog.setContent("Escribe tu correo electrónico para enviarte un e-mail de recuperación.");
-                                    dialog.getActionButton(DialogAction.POSITIVE).setEnabled(true);
-                                }
-                            }
-                        })
-                        .dismissListener(new DialogInterface.OnDismissListener() {
-                            @Override
-                            public void onDismiss(DialogInterface dialog) {
-                                Toast.makeText(getApplicationContext(), "Envía correo", Toast.LENGTH_LONG).show();
-                            }
-                        })
-                        .theme(Theme.LIGHT)
-                        .show();
-
-//                final AlertDialog.Builder builder = new AlertDialog.Builder(this);
-//                final View dialogView = getLayoutInflater().inflate(R.layout.dialog_reset_password, null);
-//                builder.setView(dialogView);
-//
-//                editTextEmailRestablece = (EditText) dialogView.findViewById(R.id.dialog_edit_text_recover_email);
-//                TextView textViewCancelar = (TextView) dialogView.findViewById(R.id.button_dialog_cancel);
-//                TextView textViewRestablecer = (TextView) dialogView.findViewById(R.id.button_dialog_confirm);
-//
-//                textViewCancelar.setOnClickListener(new View.OnClickListener() {
-//                    @Override
-//                    public void onClick(View view) {
-//                        alertDialog.dismiss();
-//                    }
-//                });
-//
-//                textViewRestablecer.setOnClickListener(new View.OnClickListener() {
-//                    @Override
-//                    public void onClick(View view) {
-//                        if (emptyFieldDialog()) {
-//                            Toast.makeText(getApplicationContext(), "Envía correo", Toast.LENGTH_LONG).show();
-//                            alertDialog.dismiss();
-//                        }
-//                    }
-//                });
-//
-//                alertDialog = builder.create();
-//                alertDialog.setCancelable(false);
-//                alertDialog.setCanceledOnTouchOutside(false);
-//                alertDialog.show();
-
-                break;
-        }
+        Intent intentHome = new Intent(this, Home.class);
+        startActivity(intentHome);
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        callbackManager.onActivityResult(requestCode, resultCode, data);
+    @OnClick(R.id.textView_nueva_cuenta)
+    public void nuevaCuenta() {
+        Intent intentNuevaCuenta = new Intent(this, NuevaCuenta.class);
+        startActivity(intentNuevaCuenta);
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        // Logs 'install' and 'app activate' App Events.
-        AppEventsLogger.activateApp(this);
+    @OnClick(R.id.textView_olvida_password)
+    public void showDialogOlvidaPassword() {
+        new MaterialDialog.Builder(this)
+                .title(R.string.DIALOG_TITULO_RESTABLECE_PASSWORD)
+                .content("Escribe tu correo electrónico para enviarte un e-mail de recuperación.")
+                .inputType(InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS)
+                .positiveText(R.string.BUTTON_RESTABLECER)
+                .positiveColorRes(R.color.primary)
+                .alwaysCallInputCallback() // this forces the callback to be invoked with every input change
+                .input(R.string.HINT_EMAIL, 0, false, new MaterialDialog.InputCallback() {
+                    @Override
+                    public void onInput(MaterialDialog dialog, CharSequence input) {
+                        if (emptyFieldDialog(input.toString())) {
+                            dialog.setContent(R.string.INPUT_ERROR_CORREO_INVALIDO);
+                            dialog.getActionButton(DialogAction.POSITIVE).setEnabled(false);
+                        } else {
+                            dialog.setContent("Escribe tu correo electrónico para enviarte un e-mail de recuperación.");
+                            dialog.getActionButton(DialogAction.POSITIVE).setEnabled(true);
+                        }
+                    }
+                })
+                .onPositive(new MaterialDialog.SingleButtonCallback() {
+                    @Override
+                    public void onClick(@NonNull MaterialDialog dialog, @NonNull DialogAction which) {
+                        Toast.makeText(getApplicationContext(), "Entro "+dialog.getInputEditText().getText().toString(), Toast.LENGTH_LONG).show();
+                    }
+                })
+                .theme(Theme.LIGHT)
+                .show();
     }
 
-    @Override
-    protected void onPause() {
-        super.onPause();
-        // Logs 'app deactivate' App Event.
-        AppEventsLogger.deactivateApp(this);
-    }
 
-    public boolean emptyFieldDialog() {
-        if (Utils.checkEditTextNotEmpty(this.editTextEmailRestablece)) {
-            if (!DataValidator.isValidEmail(this.editTextEmailRestablece.getText().toString())) {
-                this.editTextEmailRestablece.setError(Utils.stringFromResource(getApplicationContext(), R.string.INPUT_ERROR_CORREO_INVALIDO));
-                return false;
-            }
-        } else {
-            this.editTextEmailRestablece.setError(Utils.stringFromResource(Login.this, R.string.CAMPO_NO_PUEDE_SER_VACIO));
-            return false;
-        }
-        return true;
-    }
+//    public boolean emptyFieldDialog() {
+//        if (Utils.checkEditTextNotEmpty(this.editTextEmailRestablece)) {
+//            if (!DataValidator.isValidEmail(this.editTextEmailRestablece.getText().toString())) {
+//                this.editTextEmailRestablece.setError(Utils.stringFromResource(getApplicationContext(), R.string.INPUT_ERROR_CORREO_INVALIDO));
+//                return false;
+//            }
+//        } else {
+//            this.editTextEmailRestablece.setError(Utils.stringFromResource(Login.this, R.string.CAMPO_NO_PUEDE_SER_VACIO));
+//            return false;
+//        }
+//        return true;
+//    }
 
     public boolean emptyFieldDialog(String string) {
         return string.isEmpty() || !DataValidator.isValidEmail(string);
